a9dcd1e4dd5400bd6a679d56cefdbed3edf84ed7
cristianmeelo <cristianmeelo@gmail.com>
2023-11-13 03:27:06 -0300

    ...

--- src/app/[lang]/assets/page.tsx
+++ src/app/[lang]/assets/page.tsx
@@ -1,9 +1,25 @@
 'use client';
 
 import { Assets as AssetsView } from '@/components';
+import { useEffect } from 'react';
 
 export default function Assets({ params }: PageProps) {
   const language = params.lang;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://my-json-server.typicode.com/tractian/fake-api/assets');
+        const data = await response.json();
+console.log(data)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
 
   return <AssetsView language={language} />;
 }
